31ae7b3fcac15ed0d0431701a319633f622b6e52
Kyle Scales <kkscales0@gmail.com>
2017-11-20 23:55:38 -0500

    initial scenes

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadLevel (string name) 
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitRequest () 
+    {
+        Application.Quit();   
+    }
+
+}
