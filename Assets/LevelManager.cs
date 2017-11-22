09d717f48b3a0a296c7d74da08276caa37395205
Kyle Scales <kkscales0@gmail.com>
2017-11-21 22:02:35 -0500

    finished main game

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+
+	int max;
+	int min;
+	int guess;
+
+    public int maxGuessesAllowed = 5;
+    public Text text;
+
+	// Use this for initialization
+	void Start () 
+    {
+        StartGame();
+	}
+
+    void StartGame () 
+    {
+        max = 1000;
+        min = 1;
+        guess = Random.Range(min, max); 
+        text.text = guess.ToString();
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		if (Input.GetKeyDown(KeyCode.UpArrow)) 
+        {
+			GuessHigher();
+		} 
+        else if (Input.GetKeyDown(KeyCode.DownArrow)) 
+        {
+            GuessLower();
+		}
+	}
+
+    public void GuessHigher ()
+    {
+        //print("up arrow pressed");
+        min = guess;
+        NextGuess();
+    }
+
+    public void GuessLower ()
+    {
+        //print("down arrow pressed");
+        max = guess;
+        NextGuess();
+    }
+
+    void NextGuess () 
+    {
+        guess = Random.Range(min, max);
+        text.text = guess.ToString();
+        maxGuessesAllowed = maxGuessesAllowed - 1;
+
+        if (maxGuessesAllowed <= 0)
+        {
+            SceneManager.LoadScene("Win");
+        }
+    }
+}
